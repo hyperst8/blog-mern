0608b39f5c66b5345bc79ea5aa88577aca88e2ac
hyperst8 <huypn82@gmail.com>
2023-09-14 16:11:13 +0200

    Add login api and connect to login page

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "asdkmvcsdkdsffdsfd";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json()); // To parse json body data
 
 mongoose.connect(
@@ -27,6 +29,22 @@ app.post("/register", async (req, res) => {
   } catch (e) {
     console.log(e);
     res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    // User is logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("Wrong credentials");
   }
 });
 
