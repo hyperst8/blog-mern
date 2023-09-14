aaa0c761666523376484db223b4ecf990d47e2ad
hyperst8 <huypn82@gmail.com>
2023-09-14 21:02:41 +0200

    Fix login page refresh

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
 
   const login = async (ev) => {
     ev.preventDefault();
@@ -17,7 +19,10 @@ const LoginPage = () => {
     });
 
     if (response.ok) {
-      setRedirect(true);
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
     } else {
       alert("Wrong credentials");
     }
