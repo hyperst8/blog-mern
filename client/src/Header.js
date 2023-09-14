e99fa9e2398bb45324ea44c822a93baed522d972
hyperst8 <huypn82@gmail.com>
2023-09-14 21:03:00 +0200

    Add conditional header menu

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 const Header = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  const logout = () => {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  };
+
+  const username = userInfo?.username;
+
   return (
     <header>
       <Link to="/" className="logo">
         MyBlog
       </Link>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username ? (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a href="/" onClick={logout}>
+              Log out
+            </a>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
