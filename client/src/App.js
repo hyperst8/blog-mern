eee5760967a5c4213d9e94eb4a5afe25698ebb37
hyperst8 <huypn82@gmail.com>
2023-09-16 12:11:50 +0200

    Add Create post page, api and model

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Layout";
 import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
