488eb2c9e74150b6fb8782e54108f6fc3eab092b
hyperst8 <huypn82@gmail.com>
2023-09-17 12:27:00 +0200

    Modify createPost and add get post

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,26 +1,21 @@
+import { formatISO9075 } from "date-fns";
 import React from "react";
 
-const Post = () => {
+const Post = ({ title, summary, cover, createdAt, author }) => {
   return (
     <div className="post">
       <div className="image">
-        <img
-          src="https://www.seriouseats.com/thmb/2vsacPff0I2gzms8xEhv-FbAlv4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__01__20140120-beef-ramyun-homemade-recipe-15-014423f0ac734abc8c0d826e54857afe.jpg"
-          alt="blog food"
-        />
+        <img src={"http://localhost:4000/" + cover} alt="blog post" />
       </div>
       <div className="texts">
-        <h2>Homemade Shin Cup-Style Spicy Korean Ramyun Beef Noodle Soup</h2>
+        <h2>{title}</h2>
         <p className="info">
           <a href="/" className="author">
-            Huy
+            {author.username}
           </a>
-          <time>2023-09-13 10:52</time>
+          <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
-        <p className="summary">
-          A luxurious, upgraded version of the classic Korean-style spicy
-          instant noodles.
-        </p>
+        <p className="summary">{summary}</p>
       </div>
     </div>
   );
